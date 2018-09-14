11e3e19d73e632e9de549c4e727f7676753c0200
root <root@localhost.localdomain>
2018-09-14 05:47:50 -0400

    Init the create secure boot key and siging file

---
+++ signing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sbsign --key DB.key --cert DB.crt --output /boot/efi/EFI/centos/shim.efi /boot/efi/EFI/centos/shim.efi
+sbsign --key KEK.key --cert KEK.crt --output /boot/efi/EFI/centos/shim.efi /boot/efi/EFI/centos/shim.efi
+sbsign --key PK.key --cert PK.crt --output /boot/efi/EFI/centos/shim.efi /boot/efi/EFI/centos/shim.efi
+sbsign --key DB.key --cert DB.crt --output /boot/efi/EFI/centos/grubaa64.efi /boot/efi/EFI/centos/grubaa64.efi
+sbsign --key KEK.key --cert KEK.crt --output /boot/efi/EFI/centos/grubaa64.efi /boot/efi/EFI/centos/grubaa64.efi
+sbsign --key PK.key --cert PK.crt --output /boot/efi/EFI/centos/grubaa64.efi /boot/efi/EFI/centos/grubaa64.efi
